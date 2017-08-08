4e94c87f67b705cd736817e444c921ac446495f8
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-08-09 01:14:37 +0530

    priority done

--- scheduling.c
+++ scheduling.c
@@ -122,6 +122,69 @@ void roundrobin(){
 
 void pri(){
 
+  int n,p[10],b[10],c[10],ta[10],wt[10],id[10],total_burst=0,i,j,temp1,temp2,val;
+
+  printf("Enter the number of processes:");
+  scanf("%d",&n);
+  printf("Enter the burst time and priority of the processes\n");
+
+  for(i=0;i<n;i++){
+    id[i]=i+1;
+    ta[i] = 0;
+    wt[i] = 0;
+    printf("Burst Time Of Process %d:",i+1);
+    scanf("%d",&b[i]);
+    total_burst+=b[i];
+    printf("Priority of Process %d:",i+1);
+    scanf("%d",&p[i]);
+    c[i] = b[i];
+    }
+
+    printf("Total Burst Time = %d\n",total_burst);
+
+    for(i=0;i<n-1;i++){
+      for(j=0;j<n-1-i;j++){
+        if(p[j] > p[j+1]){
+          temp1 = p[j];
+          p[j] = p[j+1];
+          p[j+1] = temp1;
+
+          // temp3 = b[j];
+          // b[j] = b[j+1];
+          // b[j+1] = temp3;
+
+          temp2 = id[j];
+          id[j] = id[j+1];
+          id[j+1] = temp2;
+        }
+      }
+    }
+
+    //schd(id,b,c,wt,ta,total_burst,n);
+
+
+    for(i=0;i<total_burst;i++){
+      val = 0;
+      while(b[val] == 0){
+        val++;
+      }
+      if(b[val] != 0 && val < n){
+        b[val]--;
+
+        for(j=val+1;j<n;j++){
+          wt[j]++;
+        }
+      }
+    }
+
+
+    printf("Process\tPriority\tBurst Time\tWaiting Time\tTurn Around Time\n");
+    for(i=0;i<n;i++){
+      ta[i] = c[i] + wt[i];
+      printf("%d\t%d\t\t%d\t\t%d\t\t%d\n",id[i],p[i],c[i],wt[i],ta[i]);
+    }
+
+
 }
 
 void sjf(){
